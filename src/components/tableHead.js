a7675c90245f9b3a4b2544e4740e9bf03d0f55d9
Sakiskid <tylerdrew.home@gmail.com>
2020-12-05 12:47:09 -0600

    refactored tableHead to use new getCategories method

--- src/components/tableHead.js
+++ src/components/tableHead.js
@@ -5,35 +5,21 @@ import CATEGORIES from '../data/categoriesContext';
 
 function TableHead (props) {
     function getCategories () {
-        let categories = [];
-        let tableHeaderID = 0;
-        for (const category in CATEGORIES) {
-            let thisCat = CATEGORIES[category];
-            if (thisCat.display !== null) {
-                categories.push(<TableHeader 
-                    text={thisCat.display} 
-                    category={thisCat.value} 
-                    sortMethod={props.sortMethod}
-                    key={"tableHeader-" + tableHeaderID}
-                    />)
-            }
-            tableHeaderID++;
-        }
-        return categories;
+        return CATEGORIES.getCategories(category => {
+            return (
+            <TableHeader 
+                text={category.display} 
+                category={category.value} 
+                sortMethod={props.sortMethod}
+                key={"tableHeader-" + category.value}
+                />)
+        });
     }
 
     return (
         <thead>
             <tr>
                 {getCategories()}
-                {/* <TableHeader text="Team" category="team" sortMethod={props.sortMethod}/>
-                <TableHeader text="Username" category="username" sortMethod={props.sortMethod}/>
-                <TableHeader text="First Name" category="firstName" sortMethod={props.sortMethod}/>
-                <TableHeader text="Last Name" category="lastName" sortMethod={props.sortMethod}/>
-                <TableHeader text="Nationality" category="nat" sortMethod={props.sortMethod}/>
-                <TableHeader text="Rank" category="competitiveRank" sortMethod={props.sortMethod}/>
-                <TableHeader text="Main Role" category="mainRole" sortMethod={props.sortMethod}/>
-                <TableHeader text="Main Hero" category="mainHero" sortMethod={props.sortMethod}/> */}
             </tr>
          </thead>
     )
