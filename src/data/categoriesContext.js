8c640c02f29f533d3aaee471bc1ab62127604070
Sakiskid <tylerdrew.home@gmail.com>
2020-12-05 12:49:05 -0600

    cleaned up categoriesContext

--- src/data/categoriesContext.js
+++ src/data/categoriesContext.js
@@ -51,11 +51,6 @@ const CATEGORIES = {
       value: "nat",
       path: 'nationality',
     },
-    // competitiveRank: {
-    //   display: "Rank",
-    //   value: "competitiveRank",
-    //   path: 'rank',
-    // },
     mainRole: {
       display: "Role",
       value: "mainRole",
@@ -69,48 +64,3 @@ const CATEGORIES = {
 }
 
 export default CATEGORIES;
-
-/*
-Old categories, before the refactor into a new OWL API
-const CATEGORIES = {
-    team: {
-      display: "Team",
-      value: "team",
-      path: "team",
-    },
-    username: {
-      display: "Username",
-      value: "username",
-      path: ['login', 'username'],
-    },
-    firstName : {
-      display: "First Name",
-      value: "firstName",
-      path: ['name', 'first'],
-    },
-    lastName: {
-      display: "Last Name",
-      value: "lastName",
-      path: ['name', 'last'],
-    },
-    nat: {
-      display: "Nationality",
-      value: "nat",
-      path: 'nat',
-    },
-    competitiveRank: {
-      display: "Rank",
-      value: "competitiveRank",
-      path: 'rank',
-    },
-    mainRole: {
-      display: "Role",
-      value: "mainRole",
-      path: 'role',
-    },
-    mainHero: {
-      display: "Main Hero",
-      value: "mainHero",
-      path: 'hero',
-    },
-}*/
